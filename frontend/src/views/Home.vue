23e60e58a36825e26a801deee53d92113d7c51e1
BGWake <bgwakefield42@gmail.com>
2021-04-10 23:17:53 -0400

    small typo fix

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     <br />
     <h1 class="home-header mt-5 mb-5">Welcome to The Social Node</h1>
-    <p class="home-header mb-5">You can see what others are saying through the sidebar. Log in to try out all the features! </p>
+    <p class="home-header mb-5">You can see what others are saying through the sidebar. Log in to try out all of the features! </p>
     <login />
   </div>
 </template>
